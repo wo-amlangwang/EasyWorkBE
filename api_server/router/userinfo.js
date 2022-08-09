0ae385885bf2137a2271a47cb5c8f41630444ae3
MangXu-JA <72857545+MangXu-JA@users.noreply.github.com>
2022-08-09 15:39:04 +0800

    Add files via upload

--- api_server/router/userinfo.js
+++ api_server/router/userinfo.js
@@ -9,7 +9,7 @@ const userinfo_handler = require('../router_handler/userinfo')
 // 导入验证数据的中间件
 const expressJoi = require('@escook/express-joi')
 // 导入需要的验证规则对象
-const { update_userinfo_schema, update_password_schema, update_avatar_schema } = require('../schema/user')
+const { update_userinfo_schema, update_password_schema, update_avatar_schema, get_like_user_schema } = require('../schema/user')
 
 // 获取用户基本信息的路由
 router.get('/userinfo', userinfo_handler.getUserInfo)
@@ -19,5 +19,8 @@ router.post('/userinfo', expressJoi(update_userinfo_schema), userinfo_handler.up
 router.post('/updatepwd', expressJoi(update_password_schema), userinfo_handler.updatePassword)
 // 更换头像的路由
 router.post('/update/avatar', expressJoi(update_avatar_schema), userinfo_handler.updateAvatar)
+
+// 根据用户名LIKE查找返回用户ID和用户名头像
+router.post('/getlikeuser',expressJoi(get_like_user_schema), userinfo_handler.getLikeUser)
 
 module.exports = router
