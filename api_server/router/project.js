f954e0aae8c27f5467fb4b5a34a295863ff1261e
MangXu-JA <72857545+MangXu-JA@users.noreply.github.com>
2022-08-09 15:00:23 +0800

    Add files via upload

--- api_server/router/project.js
+++ api_server/router/project.js
@@ -9,14 +9,16 @@ const project_handler = require('../router_handler/project')
 // 1. 导入验证数据的中间件
 const expressJoi = require('@escook/express-joi')
 // 2. 导入需要的验证规则对象
-const { create_schema, addmember_schema, delete_project_schema, get_project_schema, update_project_schema } = require('../schema/project')
+const { create_schema, member_schema, delete_project_schema, get_project_schema, update_project_schema } = require('../schema/project')
 
 // 创建新项目的路由
 router.post('/create', expressJoi(create_schema), project_handler.createProject)
 // 获取项目列表数据的路由
 router.get('/projectlist', project_handler.getProjectList)
 // 添加项目成员的路由
-router.post('/addmember', expressJoi(addmember_schema), project_handler.addMember)
+router.post('/addmember', expressJoi(member_schema), project_handler.addMember)
+// 删除项目成员的路由
+router.post('/deletemember', expressJoi(member_schema), project_handler.deleteMemberById)
 // 根据 项目ID 删除项目的路由
 router.post('/deleteproject', expressJoi(delete_project_schema), project_handler.deleteProjectById)
 // 根据 项目ID 查询项目基本信息的路由
